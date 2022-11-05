e58947bc2f8fc5788749d54033694f8cbf6d1ec3
David KRAME <kramedavid@gmail.com>
2022-11-05 23:11:09 +0100

    First change API-address

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -15,33 +15,36 @@ export const MY_EXTRACT_DOCX_API_WITHOUT_LINE =
 
 //API DE SYNTHESE
 
-export const API_KEY =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImRhdmlkaGlsYmVydDY2NkBnbWFpbC5jb20iLCJleHAiOjE2NjkzMTcyMjR9.7yddu1der_gxcPVQPXlr7aa7I4T6jXlcLy_INbWvvXA";
-export const ENDPOINT_GENSIM = "http://localhost:5000/extractive-gensim";
-export const ENDPOINT_BART_LARGE = "http://localhost:5000/abstractive-large";
-export const ENDPOINT_BART_VERY_LARGE =
-  "http://localhost:5000/abstractive-very-large";
-export const ENDPOINT_MERGE = "http://localhost:5000/extractive-merge";
-export const ENDPOINT_BARThez = "http://localhost:5000/abstractive-barthez";
-export const ENDPOINT_PEGASUS = "http://localhost:5000/abstractive-pegasus";
-export const ENDPOINT_BARTKrame =
-  "http://localhost:5000/abstractive-experimental-bartkrame";
-export const ENDPOINT_EVALUATION = "http://localhost:5000/evaluation";
+// export const API_KEY =
+//   "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImRhdmlkaGlsYmVydDY2NkBnbWFpbC5jb20iLCJleHAiOjE2NjkzMTcyMjR9.7yddu1der_gxcPVQPXlr7aa7I4T6jXlcLy_INbWvvXA";
+// export const ENDPOINT_GENSIM = "http://localhost:5000/extractive-gensim";
+// export const ENDPOINT_BART_LARGE = "http://localhost:5000/abstractive-large";
+// export const ENDPOINT_BART_VERY_LARGE =
+//   "http://localhost:5000/abstractive-very-large";
+// export const ENDPOINT_MERGE = "http://localhost:5000/extractive-merge";
+// export const ENDPOINT_BARThez = "http://localhost:5000/abstractive-barthez";
+// export const ENDPOINT_PEGASUS = "http://localhost:5000/abstractive-pegasus";
+// export const ENDPOINT_BARTKrame =
+//   "http://localhost:5000/abstractive-experimental-bartkrame";
+// export const ENDPOINT_EVALUATION = "http://localhost:5000/evaluation";
 
 // Vraie endpoint ici : /abstractive-experimental-BARTkrame
 // Le modèle que je vais uploader sur Hugging Face sera nommé krm/BARTkrame-abstract (DEJA FAIT!)
 
-// export const ENDPOINT_GENSIM = "http://139.177.207.23/extractive-gensim";
-// export const ENDPOINT_BART_LARGE = "http://139.177.207.23/abstractive-large";
-// export const ENDPOINT_BART_VERY_LARGE =
-//   "http://139.177.207.23/abstractive-very-large";
-// export const ENDPOINT_MERGE = "http://139.177.207.23/extractive-merge";
-// export const ENDPOINT_BARThez = "http://139.177.207.23/abstractive-barthez";
-// export const ENDPOINT_PEGASUS = "http://139.177.207.23/abstractive-pegasus";
+export const ENDPOINT_GENSIM = "http://96.126.112.158:5000/extractive-gensim";
+export const ENDPOINT_BART_LARGE =
+  "http://96.126.112.158:5000/abstractive-large";
+export const ENDPOINT_BART_VERY_LARGE =
+  "http://96.126.112.158:5000/abstractive-very-large";
+export const ENDPOINT_MERGE = "http://96.126.112.158:5000/extractive-merge";
+export const ENDPOINT_BARThez =
+  "http://96.126.112.158:5000/abstractive-barthez";
+export const ENDPOINT_PEGASUS =
+  "http://96.126.112.158:5000/abstractive-pegasus";
 
-// export const ENDPOINT_BARTKrame =
-//   "http://139.177.207.23/abstractive-experimental-bartkrame";
-// export const ENDPOINT_EVALUATION = "http://139.177.207.23/evaluation";
+export const ENDPOINT_BARTKrame =
+  "http://96.126.112.158:5000/abstractive-experimental-bartkrame";
+export const ENDPOINT_EVALUATION = "http://96.126.112.158:5000/evaluation";
 
 // Id des boutons radio (pourn ne pas se tromper en choisissant l'end-point de synthèse)
 
