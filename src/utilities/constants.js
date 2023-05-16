3c5629ea669505830eaebccdc3d55fb0b978c96a
David KRAME <kramedavid@gmail.com>
2023-05-16 22:51:25 +0200

    Renew credentials

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,48 +1,60 @@
 //MON BACKEND POUR L'EXTRACTION DES TEXTES
 
-export const MY_EXTRACT_PDF_API =
-  "https://my-docx-pdf-extractor.herokuapp.com/extractPDF";
-export const MY_EXTRACT_DOCX_API =
-  "https://my-docx-pdf-extractor.herokuapp.com/extractWORD";
+//  LOCATIONS
+// D:\US_Project\00000PRATIQUES\Interfaces\Interfaces\my-resumer
+// SANS RET : D:\US_Project\00000PRATIQUES\My_Backend_FileReader\MyReader
+// AVEC RET : D:\US_Project\00000PRATIQUES\My_Backend_FileReader\My_pdf_docx_content_extractor
+
+// AFTER HEROKU'S CHANGES
+
+// export const MY_EXTRACT_PDF_API =
+//   "https://my-docx-pdf-extractor.herokuapp.com/extractPDF";
+// export const MY_EXTRACT_DOCX_API =
+//   "https://my-docx-pdf-extractor.herokuapp.com/extractWORD";
+
+// export const MY_EXTRACT_PDF_API_WITHOUT_LINE =
+//   "https://my-docx-pdf-extractor-not-line.herokuapp.com/extractPDF";
+// export const MY_EXTRACT_DOCX_API_WITHOUT_LINE =
+//   "https://my-docx-pdf-extractor-not-line.herokuapp.com/extractWORD";
+
+export const MY_EXTRACT_PDF_API = "http://localhost:8002/extractPDF";
+export const MY_EXTRACT_DOCX_API = "http://localhost:8002/extractWORD";
 
 export const MY_EXTRACT_PDF_API_WITHOUT_LINE =
-  "https://my-docx-pdf-extractor-not-line.herokuapp.com/extractPDF";
+  "http://localhost:3333/extractPDF";
 export const MY_EXTRACT_DOCX_API_WITHOUT_LINE =
-  "https://my-docx-pdf-extractor-not-line.herokuapp.com/extractWORD";
-
-// export const MY_EXTRACT_PDF_API = "http://localhost:3333/extractPDF";
-// export const MY_EXTRACT_DOCX_API = "http://localhost:3333/extractWORD";
+  "http://localhost:3333/extractWORD";
 
 //API DE SYNTHESE
 
 export const API_KEY =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImRhdmlkaGlsYmVydDY2NkBnbWFpbC5jb20iLCJleHAiOjE2NjkzMTcyMjR9.7yddu1der_gxcPVQPXlr7aa7I4T6jXlcLy_INbWvvXA";
-// export const ENDPOINT_GENSIM = "http://localhost:5000/extractive-gensim";
-// export const ENDPOINT_BART_LARGE = "http://localhost:5000/abstractive-large";
-// export const ENDPOINT_BART_VERY_LARGE =
-//   "http://localhost:5000/abstractive-very-large";
-// export const ENDPOINT_MERGE = "http://localhost:5000/extractive-merge";
-// export const ENDPOINT_BARThez = "http://localhost:5000/abstractive-barthez";
-// export const ENDPOINT_PEGASUS = "http://localhost:5000/abstractive-pegasus";
-// export const ENDPOINT_BARTKrame =
-//   "http://localhost:5000/abstractive-experimental-bartkrame";
-// export const ENDPOINT_EVALUATION = "http://localhost:5000/evaluation";
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6ImtyYW1lZGF2aWQ3QGdtYWlsLmNvbSIsImV4cCI6MTY4NDMxNTkyOX0.ALL0w2uc5MaXUBodWLtN9gelFqnO4UCrJb35mE-gve4";
+export const ENDPOINT_GENSIM = "http://localhost:8000/extractive-gensim";
+export const ENDPOINT_BART_LARGE = "http://localhost:8000/abstractive-large";
+export const ENDPOINT_BART_VERY_LARGE =
+  "http://localhost:8000/abstractive-very-large";
+export const ENDPOINT_MERGE = "http://localhost:8000/extractive-merge";
+export const ENDPOINT_BARThez = "http://localhost:8000/abstractive-barthez";
+export const ENDPOINT_PEGASUS = "http://localhost:8000/abstractive-pegasus";
+export const ENDPOINT_BARTKrame =
+  "http://localhost:8000/abstractive-experimental-bartkrame";
+export const ENDPOINT_EVALUATION = "http://localhost:8000/evaluation";
 
 // LES BARTkrame, BARThez,... ne marcheront pas à cause du PORT.
 
-export const ENDPOINT_GENSIM = "http://96.126.112.158/extractive-gensim";
-export const ENDPOINT_BART_LARGE = "http://96.126.112.158/abstractive-large";
-export const ENDPOINT_BART_VERY_LARGE =
-  "http://96.126.112.158/abstractive-very-large";
-export const ENDPOINT_MERGE = "http://96.126.112.158/extractive-merge";
-export const ENDPOINT_BARThez =
-  "http://96.126.112.158:5000/abstractive-barthez";
-export const ENDPOINT_PEGASUS =
-  "http://96.126.112.158:5000/abstractive-pegasus";
+// export const ENDPOINT_GENSIM = "http://96.126.112.158/extractive-gensim";
+// export const ENDPOINT_BART_LARGE = "http://96.126.112.158/abstractive-large";
+// export const ENDPOINT_BART_VERY_LARGE =
+//   "http://96.126.112.158/abstractive-very-large";
+// export const ENDPOINT_MERGE = "http://96.126.112.158/extractive-merge";
+// export const ENDPOINT_BARThez =
+//   "http://96.126.112.158:5000/abstractive-barthez";
+// export const ENDPOINT_PEGASUS =
+//   "http://96.126.112.158:5000/abstractive-pegasus";
 
-export const ENDPOINT_BARTKrame =
-  "http://96.126.112.158:5000/abstractive-experimental-bartkrame";
-export const ENDPOINT_EVALUATION = "http://96.126.112.158/evaluation";
+// export const ENDPOINT_BARTKrame =
+//   "http://96.126.112.158:5000/abstractive-experimental-bartkrame";
+// export const ENDPOINT_EVALUATION = "http://96.126.112.158/evaluation";
 
 // Id des boutons radio (pourn ne pas se tromper en choisissant l'end-point de synthèse)
 
